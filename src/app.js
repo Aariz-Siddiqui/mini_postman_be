f1f9136295f8f65c5c2a58e358828519fe202a83
Aariz Siddiqui <arizsiddiqui93@email.com>
2026-01-01 21:45:07 +0530

    fourth commit

--- src/app.js
+++ src/app.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./utils/db');
 
 const executeRoutes = require('./routes/executeRoutes');
-const authRoutes = require('./routes/authRoutes'); // ✅ ADD THIS
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// routes
-app.use('/api/execute', executeRoutes);
-app.use('/api/auth', authRoutes); // ✅ ADD THIS
+// 🔥 connect DB once per cold start
+connectDB();
 
-// health check
+app.use('/api/execute', executeRoutes);
+app.use('/api/auth', authRoutes);
+
 app.get('/', (req, res) => {
   res.send('Backend is running');
 });
